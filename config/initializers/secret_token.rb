6da7a1e2e88e732a5f3a3e7d7757840c0b57445b
Stephen Smithwick <stephen.smithwick@gmail.com>
2012-02-19 15:23:03 +1100

    Starting the project as basic rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HacktivityStream::Application.config.secret_token = '8871e5b4513c0f688f0df22924ff4238f69539f9229115a2b6f5066b572718b31d72d2b4c0f3317c9542fedbbb264d53ca5e92b9ad769b1439a13792ad7c3627'
