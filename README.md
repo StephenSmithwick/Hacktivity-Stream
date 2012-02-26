a3c98bf79a23bf89e953d59d551fff2ea78fb2ec
Ren Shao <renshao@gmail.com>
2012-02-26 11:48:28 +1100

    list last 300 revisions

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 **Hacktivity-Stream** is a small rails project to display the recent activity in a repository.  The sort of screen any developer can glance at in a project to get an idea what is currently happening to the source.
 
-Environment
+Dev Environment
 
 * Ruby 1.9.3
 * rails 3.2
+
+* bundle install
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,25 @@
+require 'grit'
+
 class HomeController < ApplicationController
+  def index
+
+    repo = Grit::Repo.new("~/hacktivity-git-repos")
+    @commits = repo.commits('master', 300).map do |commit|
+      {:author => commit.author.name,
+       :date => commit.committed_date,
+       :message => trim_git_svn_msg(commit.message),
+       :svn_rev => svn_revision(commit.message)
+      }
+    end
+  end
+
+  def trim_git_svn_msg(commit_msg)
+    commit_msg.gsub(/\n\ngit\-svn\-id\: .*$/m, "")
+  end
+
+  def svn_revision(commit_msg)
+    commit_msg.match(/git-svn-id: .+@(\d+) /)[1]
+  end
+
 
 end
